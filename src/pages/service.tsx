afa11db9042fe0260f45307e725e9eca54593560
Shreynik00 <jainsy@rknec.edu>
2025-12-30 21:42:15 +0530

    Update service.tsx

--- src/pages/service.tsx
+++ src/pages/service.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./css/Service.css";
+//import "./css/Service.css";
 
 function Login() {
   const [user, setUser] = useState(null);
